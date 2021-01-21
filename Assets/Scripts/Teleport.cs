39b5c43718159df640c05604b2cec2b84de806a4
frozentiger279 <55792108+frozentiger279@users.noreply.github.com>
2021-01-21 15:31:59 -0400

    Initial commit

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject Destination;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Vector3 translation =  Destination.transform.position - other.transform.position;
+            other.GetComponent<CharacterController>().Move(translation);
+        }
+    }
+}
